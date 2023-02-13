835f690480e34f185ef060458c6487896abc3d66
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-02-13 18:53:32 +0530

    commit :: implemeted swagger-ui

--- src/modules/event/event.controller.ts
+++ src/modules/event/event.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { EventService } from './event.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
+import { ApiTags } from '@nestjs/swagger/dist';
 
+@ApiTags('event-controllers')
 @Controller('event')
 export class EventController {
   constructor(private readonly eventService: EventService) {}

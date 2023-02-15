6b6010e3fc36679ef6d956ece416d960e8650dae
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-02-15 16:07:34 +0530

    commit ::done with event entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,19 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventEntity } from './modules/event/entities/event.entity';
 import { EventModule } from './modules/event/event.module';
 
 @Module({
   imports: [EventModule , TypeOrmModule.forRoot({
     type:'postgres',
-    host:'127.0.0.1',
+    host:'localhost',
     port:5432,
     username:'postgres',
     password:'Admin@123',
-    database:'postgres'
-
+    database:'postgres',
+    entities:[EventEntity],
+    synchronize:true
   })],
   controllers: [AppController],
   providers: [AppService],
--- src/modules/event/entities/event.entity.ts
+++ src/modules/event/entities/event.entity.ts
@@ -6,18 +6,31 @@ import { PrimaryGeneratedColumn } from 'typeorm/decorator/columns/PrimaryGenerat
 @Entity({ name: 'event' })
 export class EventEntity {
   @ApiProperty({ description: 'Primary key as User Id', example: 1 })
-  @Column()
+  @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({
+    description: 'Here add event name',
+    example: 'Birthday celebration',
+  })
   @Column()
   name: string;
 
+  @ApiProperty({
+    description: 'give a small description',
+    example: 'there is a celebration for birthday',
+  })
   @Column()
   description: string;
 
+  @ApiProperty({ description: 'date of event', example: '12-05-22' })
   @Column()
   when: Date;
 
+  @ApiProperty({
+    description: 'add event location here ',
+    example: 'Hazaribagh',
+  })
   @Column()
   address: string;
 }

1c08135a0c9d6adc6714df48f8fdef48691efbd9
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-02-15 17:52:22 +0530

    commit :: validations

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder } from '@nestjs/swagger';
 import { SwaggerModule } from '@nestjs/swagger/dist';
@@ -6,6 +7,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(new ValidationPipe())
   const config = new DocumentBuilder()
     .setTitle('Event-manager')
     .setDescription('In this API there will be a user and there events')
--- src/modules/event/dto/create-event.dto.ts
+++ src/modules/event/dto/create-event.dto.ts
@@ -1,6 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsDateString, Length } from 'class-validator';
 export class CreateEventDto {
+  @ApiProperty({
+    description: 'Here add event name',
+    example: 'Birthday celebration',
+  })
+  @IsString()
+  @Length(5, 255)
   readonly name: string;
+
+  @ApiProperty({
+    description: 'give a small description',
+    example: 'there is a celebration for birthday',
+  })
+  @Length(5, 255)
   readonly description: string;
-  readonly when: Date;
+
+  @ApiProperty({ description: 'date of event', example: '12-05-22' })
+  @IsDateString()
+  readonly when: string;
+
+  @ApiProperty({
+    description: 'add event location here ',
+    example: 'Hazaribagh',
+  })
+  @Length(5, 255)
   readonly address: string;
 }
